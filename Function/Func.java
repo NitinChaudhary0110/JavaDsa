860264112e23dab08eece57fa9c84971101331a2
NitinChaudhary0110 <chaudharynitin7535@gmail.com>
2025-07-24 16:34:37 +0530

    function print

---
+++ Function/Func.java
@@ -0,0 +1,13 @@
+package Function;
+
+public class Func {
+
+
+    public static void print(String name){
+         System.out.print(name);
+    }
+    public static void main(String args[]){
+        String name="nitin";
+        print(name);
+    }
+}
